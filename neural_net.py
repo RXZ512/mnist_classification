fb5b8248bb12856c013899138800661e251edf28
Rzchallenger <13631551410@163.com>
2018-08-25 15:04:23 +0800

    Update neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -1,27 +1,61 @@
-#importing data & tensorflow
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import tensorflow as tf
-#create interactive session & writer
-sess = tf.InteractiveSession()
-writer = tf.summary.FileWriter('./graphs/mnist', tf.get_default_graph())
-#softmax regression model
-x = tf.placeholder(tf.float32, shape=[None, 784], name='input')
-y_ = tf.placeholder(tf.float32, shape=[None, 10], name='output')
-W = tf.Variable(tf.zeros([784, 10]), name='weights')
-b = tf.Variable(tf.zeros([10]), name='biases')
-sess.run(tf.global_variables_initializer())
-y = tf.nn.softmax(tf.matmul(x, W) + b, name='prediction')
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
-				labels=y_, logits=y, name='cross_entropy'))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
-#trainings
-for _ in range(1000):
-	batch = mnist.train.next_batch(100)
-	sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
-#evaluating our model
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-writer.close()
-# Opening tensorboard: tensorboard --logdir="./graphs/mnist" --port 6006
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
+print(mnist.train.images)
+print(mnist.train.labels)
+print(mnist.train.images.shape)
+import pylab
+im = mnist.train.images[0]
+im = im.reshape(-1, 28)
+pylab.imshow(im)
+pylab.show()
+tf.reset_default_graph()
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+pred = tf.nn.softmax(tf.matmul(x, W) + b)
+cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
+lr = 0.01
+optimizer = tf.train.GradientDescentOptimizer(lr).minimize(cost)
+epoches = 20
+batch_size = 100
+display_step = 1
+saver = tf.train.Saver()
+model_path = 'log/mnistreg.ckpt'
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for epoch in range(epoches):
+        avg_cost = 0.
+        total_batch = int(mnist.train.num_examples/batch_size)
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            _, c = sess.run([optimizer, cost], {x: batch_xs, y: batch_ys})
+            avg_cost += c/total_batch
+        if (epoch+1) % display_step == 0:
+            print('Epoch:', '%04d' % (epoch+1), 'cost=', '{:.9f}'.format(avg_cost))
+    print('finished...yay')
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print('Accuracy:', accuracy.eval({x: mnist.test.images, y:mnist.test.labels}))
+    save_path = saver.save(sess, model_path)
+
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    saver.restore(sess, model_path)
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print('Accuracy:', accuracy.eval({x: mnist.test.images, y:mnist.test.labels}))
+    output = tf.argmax(pred, 1)
+    batch_xs, batch_ys = mnist.train.next_batch(2)
+    outputval, predv = sess.run([output, pred], {x: batch_xs, y: batch_ys})
+    print(outputval, predv, batch_ys)
+    im = batch_xs[0]
+    im = im.reshape(-1, 28)
+    pylab.imshow(im)
+    pylab.show()
+    im = batch_xs[1]
+    im = im.reshape(-1, 28)
+    pylab.imshow(im)
+    pylab.show()
